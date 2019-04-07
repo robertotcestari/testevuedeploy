10574a98d4ae8213b14ed4f1ede4185aa8ec7734
Roberto Cestari <robertotcestari@gmail.com>
2019-04-07 19:26:30 -0300

    tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Beto Cestari Teste"/> -->
+    <h1 class="tw-text-red">Esse é o meu Site do Beto</h1>
+    <p
+      class="tw-text-grey-darker"
+    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam deleniti sed iste ad quos iusto hic delectus, repudiandae a enim eligendi magni nihil mollitia, amet dicta ea illum quia voluptates.</p>
+
+    <img src="./assets/img/beto.png" alt>
   </div>
 </template>
 
